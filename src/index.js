f78a3781e7f7f50941d2c3e960f67378c849960b
GaganShivanna <98219082+GaganShivanna@users.noreply.github.com>
2024-07-26 19:08:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
+import  ThemeContextProvider  from '../../landing-page-template/src/components/ThemeContext';
+
 
 const domain = process.env.REACT_APP_AUTH_DOMAIN;
 const client = process.env.REACT_APP_AUTH_CLIENT_ID;
@@ -13,7 +15,9 @@ root.render(
      domain={domain}
      clientId={client}
      authorizationParams={{ redirect_uri:window.location.origin}}>
-    <App />
+     <ThemeContextProvider> 
+        <App />
+      </ThemeContextProvider>
   </Auth0Provider>
   </React.StrictMode>
 );
